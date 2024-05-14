8a0e9cdbde7e264eae63b6306c2f65daa6fe1016
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-05-14 23:08:52 +0600

    btn hover

--- checkComment.js
+++ checkComment.js
@@ -1,15 +1,17 @@
-const commentString = "this";
-console.log(commentString);
+const commentString = "th0000000000        is .";
+// console.log(commentString);
 const lengthOfCommentString = commentString.length;
-console.log(
-  lengthOfCommentString,
-  commentString[0],
-  commentString[1],
-  commentString[3 - 1]
-);
 const firstString = commentString[0];
 const secondString = commentString[1];
 const thirdString = commentString[2];
 const nThMinusTwoString = commentString[lengthOfCommentString - 2];
 const lastString = commentString[lengthOfCommentString - 1];
-console.log(nThMinusTwoString, lastString);
+// console.log(nThMinusTwoString, lastString);
+
+const clickCheck = document.addEventListener("click", function () {
+  console.log("clicked");
+  const commentString = document.getElementById("comments").value;
+  //   const commentStringText = commentString.value;
+  console.log("value is", commentString);
+  if()
+});
